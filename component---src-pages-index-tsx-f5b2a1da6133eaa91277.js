0af36f4d86a2fd0811f39088182d72991e954644
vincenterc <chung.menghsun@gmail.com>
2019-12-29 11:14:53 +0800

    Updates

---
+++ component---src-pages-index-tsx-f5b2a1da6133eaa91277.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{QeBL:function(t,e,n){"use strict";n.r(e);var r=n("q1tI"),a=n.n(r);e.default=function(){return a.a.createElement("h1",null,"Gatsby starter with typescript!")}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-f5b2a1da6133eaa91277.js.map
